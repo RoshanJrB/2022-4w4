5c952c25d42c40a5a73e752b2f35ffa8a389a81d
Roshan Jr Bussun <64860415+RoshanJrB@users.noreply.github.com>
2022-04-01 18:09:11 -0400

    commit pour le lab5

--- functions.php
+++ functions.php
@@ -1,8 +1,24 @@
 <?php 
 
+/* --------------------------------------------------------------------  add_theme_support() */
+function cidw_4w4_add_theme_support(){
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'custom-logo', array(
+    'height' => 100,
+    'width'  => 100,
+) );
+}
+
+add_action('after_setup_theme', 'cidw_4w4_add_theme_support');
+/* ------------------------------------------------------------------------------------------ */
+
 function cidw_4w4_enqueue(){
     //wp_enqueue_style('style_css', get_stylesheet_uri());
-    wp_enqueue_style('4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
+    wp_enqueue_style('cidw-4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
+
+    wp_enqueue_style('cidw-4w4-google-font',"https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Poppins:wght@300;400;500&family=Roboto&display=swap", false);
+
+
 }
 
 add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
@@ -12,6 +28,9 @@ function cidw_4w4_register_nav_menu(){
     register_nav_menus( array(
         'menu_principal' => __( 'Menu principal', 'cidw_4w4' ),
         'menu_footer'  => __( 'Menu footer', 'cidw_4w4' ),
+        'footer_colonne'  => __( 'Menu footer colonne', 'cidw_4w4' ),
+        'menu_cours'  => __( 'Menu cours', 'cidw_4w4' ),
+
     ) );
 }
 add_action( 'after_setup_theme', 'cidw_4w4_register_nav_menu', 0 );
@@ -29,26 +48,31 @@ function cidw_4w4_filtre_choix_menu($obj_menu){
     return $obj_menu;
 }
 add_filter("wp_nav_menu_objects","cidw_4w4_filtre_choix_menu");
-/* --------------------------------------------------------------------------  add_theme_support */
-function cidw_4w4_add_theme_support()
-{
-    add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'custom-logo', array(
-        'height' => 200,
-        'width'  => 200,
-) );
-}
-add_action( 'after_setup_theme', 'cidw_4w4_add_theme_support' );
 
-/* ----------------------------------------------------------------------  Enregistrement des sidebars */
+
+
+/* -------------------------------------------------------------------- Enregistrement des sidebar */
 add_action( 'widgets_init', 'my_register_sidebars' );
 function my_register_sidebars() {
     /* Register the 'primary' sidebar. */
     register_sidebar(
         array(
+            'id'            => 'entete_1',
+            'name'          => __( 'Entete 1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans l\'entête du site' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+
+
+    register_sidebar(
+        array(
             'id'            => 'footer_colonne_1',
-            'name'          => __( 'Footer colonne #1' ),
-            'description'   => __( 'Sidebar s\'affichant dans une colonne du pied de page' ),
+            'name'          => __( 'Footer colonne 1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une colonne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
@@ -59,8 +83,8 @@ function my_register_sidebars() {
     register_sidebar(
         array(
             'id'            => 'footer_colonne_2',
-            'name'          => __( 'Footer colonne #2' ),
-            'description'   => __( 'Sidebar s\'affichant dans une colonne du pied de page' ),
+            'name'          => __( 'Footer colonne 2' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une colonne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
@@ -71,8 +95,8 @@ function my_register_sidebars() {
     register_sidebar(
         array(
             'id'            => 'footer_colonne_3',
-            'name'          => __( 'Footer colonne #3' ),
-            'description'   => __( 'Sidebar s\'affichant dans une colonne du pied de page' ),
+            'name'          => __( 'Footer colonne 3' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une colonne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
@@ -83,8 +107,8 @@ function my_register_sidebars() {
     register_sidebar(
         array(
             'id'            => 'footer_ligne_1',
-            'name'          => __( 'Footer ligne #1' ),
-            'description'   => __( 'Sidebar s\'affichant dans une ligne du pied de page' ),
+            'name'          => __( 'Footer ligne 1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une ligne du pied de page' ),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
             'before_title'  => '<h3 class="widget-title">',
@@ -92,18 +116,12 @@ function my_register_sidebars() {
         )
     );
 
-    register_sidebar(
-        array(
-            'id'            => 'footer_ligne_2',
-            'name'          => __( 'Footer ligne #2' ),
-            'description'   => __( 'Sidebar s\'affichant dans une ligne du pied de page' ),
-            'before_widget' => '<div id="%1$s" class="widget %2$s">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h3 class="widget-title">',
-            'after_title'   => '</h3>',
-        )
-    );
     /* Repeat register_sidebar() code for additional sidebars. */
 }
 
+
+
 ?>
+© 2022 GitHub, Inc.
+Terms
+Privacy
