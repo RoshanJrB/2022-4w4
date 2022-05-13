423add0ad16d3d6c11071599059432b11f25bc70
Roshan Jr Bussun <64860415+RoshanJrB@users.noreply.github.com>
2022-05-13 15:24:14 -0400

    page evenement modification de l'apparence

--- template-evenement.php
+++ template-evenement.php
@@ -9,17 +9,30 @@
 
 <?php get_header() ?>
 <main class="site__main">
-    <h1>---- Template evenement ------</h1>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php $image = get_field('image');?>
         <img src="<?php echo esc_url($image['url']); ?>" alt="">
-        <h3>l'endroit</h3>
-        <p><?php the_field('endroit'); ?></p>
-        <p>Date de l'évènement : <?php the_field('date'); ?></p>
-        <p>Heure de l'évènement : <?php the_field('heure'); ?></p>
-        <h3>Organisateur</h3>
-        <p><?php the_field('organisateur'); ?></p>        
+        <div class="container__info">
+          <div class="date">
+               <h3>Date de l'évènement :</h3>
+               <p><?php the_field('date'); ?></p>
+          </div>
+          <div class="heure">
+               <h3>Heure de l'évènement :</h3>
+               <p><?php the_field('heure'); ?></p>
+          </div>
+          <div class="endroit">
+               <h3>l'endroit</h3>
+               <p><?php the_field('endroit'); ?></p>
+          </div>
+          <div class="organisateur">
+               <h3>Organisateur</h3>
+               <p><?php the_field('organisateur'); ?></p>
+          </div>
+        </div>
+        
+        <h1>Journée d'Accueil</h1>        
    <?php endif ?>
 </main>
 <?php get_footer() ?>
