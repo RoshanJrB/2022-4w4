f37bd8dfc223bfabf4424948f8279ec1bb7d676a
Roshan Jr Bussun <64860415+RoshanJrB@users.noreply.github.com>
2022-05-13 18:23:33 -0400

    ajout d'une animation avec un background image sur la nav de la page d'accueil

--- template-evenement.php
+++ template-evenement.php
@@ -15,24 +15,22 @@
         <img src="<?php echo esc_url($image['url']); ?>" alt="">
         <div class="container__info">
           <div class="date">
-               <h3>Date de l'évènement :</h3>
-               <p><?php the_field('date'); ?></p>
+               <h3 class="evenement__h3">Date de l'évènement :</h3>
+               <p class="evenement__p"><?php the_field('date'); ?></p>
           </div>
           <div class="heure">
-               <h3>Heure de l'évènement :</h3>
-               <p><?php the_field('heure'); ?></p>
+               <h3 class="evenement__h3">Heure de l'évènement :</h3>
+               <p class="evenement__p"><?php the_field('heure'); ?></p>
           </div>
           <div class="endroit">
-               <h3>l'endroit</h3>
-               <p><?php the_field('endroit'); ?></p>
+               <h3 class="evenement__h3">l'endroit</h3>
+               <p class="evenement__p"><?php the_field('endroit'); ?></p>
           </div>
           <div class="organisateur">
-               <h3>Organisateur</h3>
-               <p><?php the_field('organisateur'); ?></p>
+               <h3 class="evenement__h3">Organisateur</h3>
+               <p class="evenement__p"><?php the_field('organisateur'); ?></p>
           </div>
-        </div>
-        
-        <h1>Journée d'Accueil</h1>        
+        </div>        
    <?php endif ?>
 </main>
 <?php get_footer() ?>
